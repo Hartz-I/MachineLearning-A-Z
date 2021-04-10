ab6f0565a011a40cf7c7ffbd8c46bb7a6523fa61
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-04-10 11:05:12 +0600

    learned to take care of missing data

--- 1. Data Preprossesing/Section 3 - Data Preprocessing in Python/Python/data_preprocessing_template.py
+++ 1. Data Preprossesing/Section 3 - Data Preprocessing in Python/Python/data_preprocessing_template.py
@@ -9,3 +9,18 @@ import pandas as pd #to import and manage datasets
 #this has to be in the same folder with dataset to that folder be working directory
 
 dataset = pd.read_csv("Data.csv")
+X= dataset.iloc[: , :-1].values #row this to that , col this to that. -1 means all but last one
+Y= dataset.iloc[: , 3].values #taking 4th column
+
+#handling missing data
+#take the mean of the columns put on missing place
+
+#what missing values are we working with, what to do with them , 0=column mean and 1=row mean but axis not needed column automatic
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan , strategy = 'mean') #instead of what was said
+#has to be np.nan
+
+imputer = imputer.fit(X[: , 1:3]) #fitting into all the rows and 2nd and 3rd column . 3 cuz upper bound excluded
+
+X[: , 1:3] = imputer.transform(X[: , 1:3]) #replacing the column with mean encluded columns
+
--- 1. Data Preprossesing/Section 4 - Data Preprocessing in R/R/data_preprocessing_template.R
+++ 1. Data Preprossesing/Section 4 - Data Preprocessing in R/R/data_preprocessing_template.R
@@ -1,1 +1,13 @@
 #Data preprocessing
+
+#importing dataset
+dataset = read.csv('Data.csv') #must be single quote #added data into dataset variable
+
+#ifelse(condtion, if true, if false)
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), #replacing with mean
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), #replacing with mean
+                     dataset$Salary)
